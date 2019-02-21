21eca04bd26d6e2b94cd5178636c43027c4c1b8d
Mikael Samvelian <mikael.samvelian@gmail.com>
2019-02-20 20:40:45 -0500

    Add schema for graphql
    This commit adds a simple schema for graphql to get started on testing querying and mutating data.

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World!');
-})
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      doctors: [String!]!
+    }
+
+    type RootMutation {
+      createDoctor(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+    `),
+    rootValue: {
+      doctors: () => {
+        return ['Doctor1', 'Doctor2', 'Doctor3'];
+      },
+      createEvent: (args) => {
+        const doctorName = args.name;
+        return doctorName;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(3000);
