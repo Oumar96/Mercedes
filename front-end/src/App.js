40c69d58e54223b57833da9c2735b737c7c77057
Sebastien Ong Tone <s.ongtone@hotmail.com>
2019-03-10 21:22:25 -0400

    [#2] Added the Doctor.js page and modified the front end to have links to the other pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+import AppointmentsPage from './pages/Appointments';
+import MainNavigation from './components/Navigation/MainNavigation';
+import AuthContext from './context/auth-context';
+import BookPage from './pages/Book';
+import DoctorPage from './pages/Doctor';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    token: null,
+    userId: null
+  };
+
+  login = (token, userId, tokenExpiration) => {
+    this.setState({ token: token, userId: userId });
+  };
+
+  logout = () => {
+    this.setState({ token: null, userId: null });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+          <BrowserRouter>
+            <React.Fragment>
+              <AuthContext.Provider
+                  value={{
+                    token: this.state.token,
+                    userId: this.state.userId,
+                    login: this.login,
+                    logout: this.logout
+                  }}
+              >
+                <MainNavigation />
+                <main className="main-content">
+                  <Switch>
+                    {!this.state.token && <Redirect from="/" to="/book" exact />}
+                    {this.state.token && <Redirect from="/" to="/events" exact />}
+                    {this.state.token && (
+                        <Redirect from="/auth" to="/events" exact />
+                    )}
+                    {!this.state.token && (
+                        <Route path="/auth" component={AuthPage} />
+                    )}
+                    <Route path="/events" component={AppointmentsPage} />
+                    <Route path="/book" component={BookPage} />
+                    <Route path="/schedule" component={DoctorPage} />
+                  </Switch>
+                </main>
+              </AuthContext.Provider>
+            </React.Fragment>
+          </BrowserRouter>
+        </div>
     );
   }
 }
---
+++ front-end/src/pages/Doctor.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class DoctorPage extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <img class="image" src="https://www.sleeptmj.com/wp-content/uploads/2017/10/FAQs1.jpg" alt="clinic" />
+            </React.Fragment>
+        );
+    }
+}
+
+export default DoctorPage;
