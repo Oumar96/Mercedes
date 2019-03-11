c2b5335c72b7731d88d0667066bec2259373d111
Tyler <tdelaportas@hotmail.com>
2019-03-10 21:49:44 -0400

    Changes and to react page that communicates with back-end #20

--- front-end/src/pages/Appointments.js
+++ front-end/src/pages/Appointments.js
@@ -1,134 +1,56 @@
 import React, { Component } from 'react';
 
 import Modal from '../components/Modal/Modal';
-import Backdrop from '../components/Backdrop/Backdrop';
 import AppointmentList from '../components/Appointments/AppointmentList/AppointmentList';
 import Spinner from '../components/Spinner/Spinner';
 import AuthContext from '../context/auth-context';
 import './Appointments.css';
+import TextField from "material-ui/TextField";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
-class EventsPage extends Component {
+class AppointmentsPage extends Component {
   state = {
     creating: false,
     appointments: [],
     isLoading: false,
-    selectedAppointment: null
+    selectedAppointment: null,
+    healthCareNumber: ""
   };
   isActive = true;
 
   static contextType = AuthContext;
 
-  constructor(props) {
-    super(props);
-    this.titleElRef = React.createRef();
-    this.priceElRef = React.createRef();
-    this.dateElRef = React.createRef();
-    this.descriptionElRef = React.createRef();
-  }
-
   componentDidMount() {
+    if (this.context.token) {
+      this.fetchAppointmentsNurse();
+    } else {
     this.fetchAppointments();
+    }
   }
 
   startCreateEventHandler = () => {
     this.setState({ creating: true });
   };
 
-  modalConfirmHandler = () => {
-    this.setState({ creating: false });
-    const title = this.titleElRef.current.value;
-    const price = +this.priceElRef.current.value;
-    const date = this.dateElRef.current.value;
-    const description = this.descriptionElRef.current.value;
-
-    if (
-      title.trim().length === 0 ||
-      price <= 0 ||
-      date.trim().length === 0 ||
-      description.trim().length === 0
-    ) {
-      return;
-    }
-
-    const event = { title, price, date, description };
-    console.log(event);
-
-    const requestBody = {
-      query: `
-          mutation CreateEvent($title: String!, $desc: String!, $price: Float!, $date: String!) {
-            createEvent(eventInput: {title: $title, description: $desc, price: $price, date: $date}) {
-              _id
-              title
-              description
-              date
-              price
-            }
-          }
-        `,
-        variables: {
-          title: title,
-          desc: description,
-          price: price,
-          date: date
-        }
-    };
-
-    const token = this.context.token;
-
-    fetch('http://localhost:8000/graphql', {
-      method: 'POST',
-      body: JSON.stringify(requestBody),
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + token
-      }
-    })
-      .then(res => {
-        if (res.status !== 200 && res.status !== 201) {
-          throw new Error('Failed!');
-        }
-        return res.json();
-      })
-      .then(resData => {
-        this.setState(prevState => {
-          const updatedEvents = [...prevState.events];
-          updatedEvents.push({
-            _id: resData.data.createEvent._id,
-            title: resData.data.createEvent.title,
-            description: resData.data.createEvent.description,
-            date: resData.data.createEvent.date,
-            price: resData.data.createEvent.price,
-            creator: {
-              _id: this.context.userId
-            }
-          });
-          return { events: updatedEvents };
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-
   modalCancelHandler = () => {
     this.setState({ creating: false, selectedAppointment: null });
   };
 
-  fetchAppointments() {
+  fetchAppointmentsNurse() {
     this.setState({ isLoading: true });
     const requestBody = {
       query: `
-          query {
-            appointments {
-              _id
-              type
-              slots {
-                slot_time
-                slot_date
-              }
-            }
+      query {
+        appointments {
+          _id
+          type
+          slots {
+            slot_time
+            slot_date
           }
-        `
+        }
+      }
+      `
     };
 
     fetch('http://localhost:8000/graphql', {
@@ -158,32 +80,70 @@ class EventsPage extends Component {
       });
   }
 
-  showDetailHandler = appointmentId => {
-    this.setState(prevState => {
-      const selectedAppointment = prevState.appointments.find(a => a._id === appointmentId);
-      return { selectedAppointment: selectedAppointment };
-    });
-  };
-
-  bookEventHandler = () => {
-    if (!this.context.token) {
-      this.setState({ selectedEvent: null });
-      return;
-    }
-    console.log(this.state.selectedEvent)
+  fetchAppointments() {
+    this.setState({ isLoading: true });
     const requestBody = {
       query: `
-          mutation BookEvent($id: ID!) {
-            bookEvent(eventId: $id) {
+          query {
+            patients {
               _id
-             createdAt
-             updatedAt
+              hcn
+              createdAppointments {
+                _id
+                type
+                slots {
+                  slot_time
+                  slot_date
+                }
+              }
             }
           }
-        `,
-        variables: {
-          id: this.state.selectedEvent._id
+        `
+    };
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
         }
+        return res.json();
+      })
+      .then(resData => {
+        const givenHCN = this.state.healthCareNumber;
+        function isSelectedPatient(patient) {
+          return patient.hcn === givenHCN;
+        }
+        const index = resData.data.patients.findIndex(isSelectedPatient);
+        const appointments = resData.data.patients[index].createdAppointments;
+        if (this.isActive) {
+          this.setState({ appointments: appointments, isLoading: false });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        if (this.isActive) {
+          this.setState({ isLoading: false });
+        }
+      });
+  }
+
+  deleteAppointmentHandler = appointmentId => {
+    this.setState({ isLoading: true });
+    const requestBody = {
+      query: `
+          mutation CancelAppointment($id: ID!) {
+            cancelAppointment(appointmentId: $id)
+          }
+        `,
+      variables: {
+        id: appointmentId
+      }
     };
 
     fetch('http://localhost:8000/graphql', {
@@ -201,83 +161,76 @@ class EventsPage extends Component {
         return res.json();
       })
       .then(resData => {
-        console.log(resData);
-        this.setState({ selectedEvent: null });
+        this.setState(prevState => {
+          const updatedAppointments = prevState.appointments.filter(appointment => {
+            return appointment._id !== appointmentId;
+          });
+          return { appointments: updatedAppointments, isLoading: false };
+        });
       })
       .catch(err => {
         console.log(err);
+        this.setState({ isLoading: false });
       });
+  };
+
+  showDetailHandler = appointmentId => {
+    this.setState(prevState => {
+      const selectedAppointment = prevState.appointments.find(a => a._id === appointmentId);
+      return { selectedAppointment: selectedAppointment };
+    });
   };
 
   componentWillUnmount() {
     this.isActive = false;
   }
 
-  annualTime() {
-    return (<p>The appointment is at {(this.state.selectedAppointment.slots.slot_time - 1) + 12}:00</p>);
+  returnTime() {
+    if (this.state.selectedAppointment.type === "Non-Urgent") {
+      var quotient = Math.floor(this.state.selectedAppointment.slots.slot_time / 3);
+      var remainder = (this.state.selectedAppointment.slots.slot_time * 20) % 60;
+      if (remainder === 0) {
+        return (<p>The appointment is at {8 + quotient}:00</p>);
+      } else {
+        return (<p>The appointment is at {8 + quotient}:{remainder}</p>);
+      }
+    } else if (this.state.selectedAppointment.type === "Annual") {
+      return (<p>The appointment is at {parseFloat(this.state.selectedAppointment.slots.slot_time) + 8}:00</p>);
+    }
+  }
+
+  handleSetHCN(hcn) {
+    this.setState({ healthCareNumber: hcn });
   }
 
   render() {
     return (
       <React.Fragment>
-        {(this.state.creating || this.state.selectedEvent) && <Backdrop />}
-        {this.state.creating && (
-          <Modal
-            title="Add Event"
-            canCancel
-            canConfirm
-            onCancel={this.modalCancelHandler}
-            onConfirm={this.modalConfirmHandler}
-            confirmText="Confirm"
-          >
-            <form>
-              <div className="form-control">
-                <label htmlFor="title">Title</label>
-                <input type="text" id="title" ref={this.titleElRef} />
-              </div>
-              <div className="form-control">
-                <label htmlFor="price">Price</label>
-                <input type="number" id="price" ref={this.priceElRef} />
-              </div>
-              <div className="form-control">
-                <label htmlFor="date">Date</label>
-                <input type="datetime-local" id="date" ref={this.dateElRef} />
-              </div>
-              <div className="form-control">
-                <label htmlFor="description">Description</label>
-                <textarea
-                  id="description"
-                  rows="4"
-                  ref={this.descriptionElRef}
-                />
-              </div>
-            </form>
-          </Modal>
-        )}
         {this.state.selectedAppointment && (
           <Modal
             title={this.state.selectedAppointment.type}
             canCancel
             canConfirm
             onCancel={this.modalCancelHandler}
-            onConfirm={this.bookEventHandler}
+            onConfirm={this.modalCancelHandler}
             confirmText={this.context.token ? 'Book' : 'Confirm'}
           >
             <h1>This is an {this.state.selectedAppointment.type} appointment</h1>
-            <p>
-              The date of the appointment is {this.state.selectedAppointment.slots.slot_date}
-            </p>
-            {this.annualTime()}
+            <p>The date of the appointment is {this.state.selectedAppointment.slots.slot_date}</p>
+            {this.returnTime()}
           </Modal>
         )}
-        {this.context.token && (
-          <div className="events-control">
-            <p>Share your own Events!</p>
-            <button className="btn" onClick={this.startCreateEventHandler}>
-              Create Event
-            </button>
-          </div>
-        )}
+        {
+          <MuiThemeProvider>
+            <div class="depth">
+              <TextField
+                fullWidth={true}
+                hintText="Enter Health Care Number"
+                onChange={(evt, newValue) => {this.handleSetHCN(newValue.replace(/\s/g, '')); this.fetchAppointments();}}
+              />
+            </div>
+          </MuiThemeProvider>
+        }
         {this.state.isLoading ? (
           <Spinner />
         ) : (
@@ -285,6 +238,7 @@ class EventsPage extends Component {
             appointments={this.state.appointments}
             authUserId={this.context.userId}
             onViewDetail={this.showDetailHandler}
+            onCancel={this.deleteAppointmentHandler}
           />
         )}
       </React.Fragment>
@@ -292,4 +246,4 @@ class EventsPage extends Component {
   }
 }
 
-export default EventsPage;
+export default AppointmentsPage;
