e2afb8011a267529da8d1d349dc2d8ddbe4cd57d
Mikael Samvelian <mikael.samvelian@gmail.com>
2019-03-11 17:01:33 -0400

    Update Doctor.js
    Add the correct appointment logic to show on the doctor page. This also has an image to match the rest of the website.

--- frontend/src/pages/Doctor.js
+++ frontend/src/pages/Doctor.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import AppointmentApp from '../components/DoctorScheduleAppointmentApp';
+
 
 class DoctorPage extends Component {
 
-    render() {
-        return (
-            <React.Fragment>
-                <img class="image" src="https://www.sleeptmj.com/wp-content/uploads/2017/10/FAQs1.jpg" alt="clinic" />
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+        <React.Fragment>
+          <img class="image" src="https://tauglobal.com/wp-content/uploads/2018/05/doctors-and-densist.png" alt="clinic" />
+          <AppointmentApp></AppointmentApp>
+        </React.Fragment>
+    );
+  }
 }
 
 export default DoctorPage;
